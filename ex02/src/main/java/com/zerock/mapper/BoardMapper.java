80e08054c01b9f8ac6b4107bd82cb727ebd78055
yelo-o <flyordig@gmail.com>
2023-08-24 17:39:04 +0900

    게시판 CRUD 구현

---
+++ ex02/src/main/java/com/zerock/mapper/BoardMapper.java
@@ -0,0 +1,17 @@
+package com.zerock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.zerock.domain.BoardVO;
+
+public interface BoardMapper {
+	//@Select("select * from tbl_board")
+	public List<BoardVO> getList();
+	public void insert(BoardVO board);
+	public void insertSelectKey(BoardVO board);
+	public BoardVO read(long bno);
+	public int delete(long bno);
+	public int update(BoardVO board);
+}
---
+++ ex02/src/test/java/com/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,60 @@
+package com.zerock.mapper;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.zerock.domain.BoardVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+	
+	@Setter(onMethod_=@Autowired)
+	private BoardMapper mapper;
+
+	/*
+	@Test
+	public void testGetList() {
+		mapper.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("대강대강 제목");
+		board.setContent("대강대강 내용");
+		board.setWriter("대강대강님");
+		mapper.insert(board);
+	}
+	
+	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(3L);
+		log.info(board);
+	}
+	
+	@Test
+	public void testDelete() {
+		int n = mapper.delete(3L);
+		log.info(n +" 개의 데이터가 삭제되었습니다.");
+	}
+	 */
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		board.setBno(2L);
+		board.setTitle("제목 수정");
+		board.setContent("내용 수정");
+		board.setWriter("김민규");
+		log.info("Update lines : " + mapper.update(board));
+	}
+}
